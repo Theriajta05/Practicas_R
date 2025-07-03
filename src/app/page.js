fd1e10dce1f891454647fbf611bb27d50ab65057
Armando Jair Rivera Tinoco <s19120158@alumnos.itsur.edu.mx>
2025-07-03 14:46:24 -0600

    prueba 2

--- src/app/page.js
+++ src/app/page.js
@@ -64,7 +64,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Learning
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
